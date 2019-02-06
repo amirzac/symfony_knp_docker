cd9cffc44e7a1b60a88c572b53b8c24cd69f6171
Anton Mirzac <antonmirzac@gmail.com>
2019-02-06 09:19:40 +0100

    Test

--- apps/my-symfony-app/src/DataFixtures/CommentFixture.php
+++ apps/my-symfony-app/src/DataFixtures/CommentFixture.php
@@ -2,25 +2,29 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Comment;
-use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Article;
+use App\Entity\Comment;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
 
 class CommentFixture extends BaseFixture implements DependentFixtureInterface
 {
-    public function loadData(ObjectManager $manager)
+    protected function loadData(ObjectManager $manager)
     {
-        $this->createMany(Comment::class, 100, function (Comment $comment) {
-            $comment->setContent($this->faker->sentences(2, true));
+        $this->createMany(100, 'main_comments', function() {
+            $comment = new Comment();
+            $comment->setContent(
+                $this->faker->boolean ? $this->faker->paragraph : $this->faker->sentences(2, true)
+            );
+
             $comment->setAuthorName($this->faker->name);
             $comment->setCreatedAt($this->faker->dateTimeBetween('-1 months', '-1 seconds'));
             $comment->setIsDeleted($this->faker->boolean(20));
+            $comment->setArticle($this->getRandomReference('main_articles'));
 
-            /* @var Article $article */
-            $article = $this->getRandomReference(Article::class);
-            $comment->setArticle($article);
+            return $comment;
         });
+
         $manager->flush();
     }
 
