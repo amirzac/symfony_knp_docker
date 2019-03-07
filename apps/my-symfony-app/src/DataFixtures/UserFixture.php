733ff916a9cbc4dd81883c7b775fdb5a255e08db
Anton Mirzac <antonmirzac@gmail.com>
2019-03-07 21:32:56 +0100

    Added API token functionality

--- apps/my-symfony-app/src/DataFixtures/UserFixture.php
+++ apps/my-symfony-app/src/DataFixtures/UserFixture.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\ApiToken;
 use App\Entity\User;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -17,12 +18,18 @@ class UserFixture extends BaseFixture
 
     protected function loadData(ObjectManager $manager)
     {
-        $this->createMany(10, 'main_users', function($i) {
+        $this->createMany(10, 'main_users', function($i) use ($manager) {
             $user = new User();
             $user->setEmail(sprintf('spacebar%d@example.com', $i));
             $user->setFirstName($this->faker->firstName);
             $user->setPassword($this->userPasswordEncoder->encodePassword($user, '123456'));
             $user->setTwitterUsername($this->faker->userName);
+
+            $apiToken1 = new ApiToken($user);
+            $apiToken2 = new ApiToken($user);
+
+            $manager->persist($apiToken1);
+            $manager->persist($apiToken2);
 
             return $user;
         });
