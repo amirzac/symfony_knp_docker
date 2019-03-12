5966c0a9ffc105d9f47bb9aa6e2411dd9446792e
Anton Mirzac <antonmirzac@gmail.com>
2019-03-12 08:56:20 +0100

    Added article create form

--- apps/my-symfony-app/src/Controller/ArticleAdminController.php
+++ apps/my-symfony-app/src/Controller/ArticleAdminController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleFormType;
+use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,42 +14,47 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class ArticleAdminController extends AbstractController
 {
-//    /**
-//     * @Route("/admin/article/new", name="admin_article_new")
-//     */
-//    public function new(EntityManagerInterface $em)
-//    {
-//        $article = new Article();
-//        $article->setTitle('Light Speed Travel 2')
-//            ->setSlug('light-speed-travel-2')
-//            ->setContent('Fountain of Youth or Fallacy')
-//            ->setAuthor('Mike Ferengi')
-//            ->setImageFilename('asteroid.jpeg')
-//            ->setPublishedAt(new \DateTime())
-//        ;
-//
-//        $em->persist($article);
-////        $em->flush();
-//
-//        return new Response(sprintf(
-//            'Hiya! New Article id: #%d slug: %s',
-//            $article->getId(),
-//            $article->getSlug()
-//        ));
-//    }
+    /**
+     * @Route("/admin/article", name="admin_article_list")
+     */
+    public function list(ArticleRepository $articleRepo)
+    {
+        $articles = $articleRepo->findAll();
+
+        return $this->render('article_admin/list.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
 
     /**
      * @Route("/admin/article/new", name="admin_article_new")
      * @IsGranted("ROLE_ADMIN_ARTICLE")
      */
-    public function new(EntityManagerInterface $em)
+    public function new(EntityManagerInterface $em, Request $request)
     {
-        die('todo');
-        return new Response(sprintf(
-            'Hiya! New Article id: #%d slug: %s',
-            $article->getId(),
-            $article->getSlug()
-        ));
+
+        $form = $this->createForm(ArticleFormType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $article = new Article();
+            $article->setTitle($data['title']);
+            $article->setContent($data['content']);
+            $article->setAuthor($this->getUser());
+
+            $em->persist($article);
+            $em->flush();
+
+            $this->addFlash('success', 'Article Created! Knowledge is power!');
+
+            return $this->redirectToRoute('admin_article_list');
+        }
+
+        return $this->render('article_admin/new.html.twig', [
+            'articleForm' => $form->createView()
+        ]);
     }
     /**
      * @Route("/admin/article/{id}/edit")
